79663672efd6214041b3447661ac25bcf81fe9e9
Brandon-Harris <brandonharris177@gmail.com>
2020-07-16 17:15:26 -0600

    MVP met

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -11,48 +11,50 @@ world = World()
 
 # You may uncomment the smaller graphs for development and testing purposes.
 # map_file = "maps/test_line.txt"
-map_file = "maps/test_cross.txt"
+# map_file = "maps/test_cross.txt"
 # map_file = "maps/test_loop.txt"
 # map_file = "maps/test_loop_fork.txt"
-# map_file = "maps/main_maze.txt"
+map_file = "maps/main_maze.txt"
 
 # Loads the map into a dictionary
 room_graph=literal_eval(open(map_file, "r").read())
 world.load_graph(room_graph)
 
 # Print an ASCII map
-world.print_rooms()
+# world.print_rooms()
 
 player = Player(world.starting_room)
 
 # Fill this out with directions to walk
 # traversal_path = ['n', 'n']
 traversal_path = []
+retrace = []
 traversal_graph = {}
 visited = set()
-travel_direction = ('n', 's')
+travel_direction = ('w', 'e')
 last_room = 'Not in graph'
 
 while len(visited) < len(room_graph)-1:
-    print(len(visited))
-    print(len(room_graph))
     current_room = player.current_room.id
     exits = player.current_room.get_exits()
+    
     if current_room not in visited:
         traversal_graph[current_room] = {}
         for each_exit in exits:
             traversal_graph[current_room][each_exit] = '?'
             visited.add(current_room)
+
     if last_room in traversal_graph:
         traversal_graph[current_room][travel_direction[1]] = last_room
         traversal_graph[last_room][travel_direction[0]] = current_room
+
     untravelded_directions = []
     for key, value in traversal_graph[current_room].items():
         if value == '?':
             untravelded_directions.append(key)
-    backward_steps = -1
+
     while untravelded_directions == []:
-        last_step = traversal_path[backward_steps]
+        last_step = retrace.pop()
         if last_step == 's':
             travel_direction = ('n', 's')
         elif last_step == 'w':
@@ -67,7 +69,7 @@ while len(visited) < len(room_graph)-1:
         for key, value in traversal_graph[current_room].items():
             if value == '?':
                 untravelded_directions.append(key)
-        backward_steps -= 1
+
     while travel_direction[0] not in untravelded_directions:
         next_travel = untravelded_directions[0] 
         if next_travel == 'n':
@@ -78,13 +80,12 @@ while len(visited) < len(room_graph)-1:
             travel_direction = ('s', 'n')
         elif next_travel == 'w':
             travel_direction = ('w', 'e')
+
     last_room = current_room
     player.travel(travel_direction[0])
     traversal_path.append(travel_direction[0])
+    retrace.append(travel_direction[0])
 
-    # print(traversal_graph)
-    # print(traversal_path)
-    # print(visited)
 
 # TRAVERSAL TEST
 visited_rooms = set()
